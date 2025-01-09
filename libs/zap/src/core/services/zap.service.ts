2bac8b17bea9d4e3b2641b19448934bf36fadfb6
Andrey Neyzhmak <pe4enka519@gmail.com>
2025-01-09 17:44:06 +0300

    feat: some updates

--- libs/zap/src/core/services/zap.service.ts
+++ libs/zap/src/core/services/zap.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class ZapService {
     public getGreeting(name: string) {
-        return `Greeting from ${name}!`;
+        return `Greeting from ${name}!!!`;
     }
 }
